53086abab7a5b6eb57d65faf5037b2aafb90d2dd
Pedro Henrique <pedroohenrique2009@hotmail.com>
2026-06-26 01:49:34 -0300

    The changes were made and the icons were added.

--- app/src/main/java/com/weatherfeed/app/MainActivity.kt
+++ app/src/main/java/com/weatherfeed/app/MainActivity.kt
@@ -5,24 +5,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
         setContentView(R.layout.activity_main)
         configureNavigation()
+    }
 
-
-        }
-
-    fun configureNavigation(){
+   private fun configureNavigation(){
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
 
