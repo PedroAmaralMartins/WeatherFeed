064e4b467942cfdb79f1834f7596f7eb21944f21
Pedro Henrique <pedroohenrique2009@hotmail.com>
2026-06-30 15:11:08 -0300

    change made.

--- app/src/main/java/com/weatherfeed/app/data/remote/RetrofitClient.kt
+++ app/src/main/java/com/weatherfeed/app/data/remote/RetrofitClient.kt
@@ -1,6 +1,7 @@
 package com.weatherfeed.app.data.remote
 
 import com.weatherfeed.app.BuildConfig
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -9,7 +10,16 @@ object RetrofitClient {
     private const val BASE_URL = "https://api.openweathermap.org/"
     private val API_KEY = BuildConfig.WEATHER_API_KEY
 
-    private val api: WeatherApiService by lazy {
+    val okHTTp = OkHttpClient.Builder().addInterceptor { chain ->
+        val url = chain.request().url().newBuilder()
+            .addQueryParameter("appid", API_KEY)
+            .build()
+        chain.proceed(chain.request().newBuilder().url(url).build())
+    }
+        .build()
+
+
+    val api: WeatherApiService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
--- app/src/main/java/com/weatherfeed/app/data/repository/WeatherRepository.kt
+++ app/src/main/java/com/weatherfeed/app/data/repository/WeatherRepository.kt
@@ -31,6 +31,4 @@ class WeatherRepository(
             Result.failure(e)
         }
     }
-
-
 }
