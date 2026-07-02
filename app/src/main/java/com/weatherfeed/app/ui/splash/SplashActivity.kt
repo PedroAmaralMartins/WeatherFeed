b290d0fe663a9b418f383318a3dcb3b84f81f319
Pedro Henrique <pedroohenrique2009@hotmail.com>
2026-07-01 23:17:25 -0300

    corrections made.

--- app/src/main/java/com/weatherfeed/app/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/weatherfeed/app/ui/splash/SplashActivity.kt
@@ -5,8 +5,11 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.weatherfeed.app.MainActivity
 import com.weatherfeed.app.databinding.ActivitySplashBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
     private val binding: ActivitySplashBinding by lazy {
@@ -22,10 +25,15 @@ class SplashActivity : AppCompatActivity() {
         binding.icFirstScreen.animate().alpha(1f).setDuration(800).start()
         binding.titleFirstScreen.animate().alpha(1f).setStartDelay(400).setDuration(600).start()
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
+        lifecycleScope.launch {
+            delay(2200)
+            startActivity(
+                Intent(
+                    this@SplashActivity,
+                    MainActivity::class.java
+                )
+            )
             finish()
-        }, 2200)
-
+        }
     }
 }
