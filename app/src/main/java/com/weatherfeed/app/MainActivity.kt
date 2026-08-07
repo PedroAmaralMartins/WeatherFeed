54f4e19479a4c492195a51b8bd1eed99718a76ca
Pedro Henrique <pedroohenrique2009@hotmail.com>
2026-08-07 10:33:15 -0300

    The backstack of the bottom nav was corrected.

--- app/src/main/java/com/weatherfeed/app/MainActivity.kt
+++ app/src/main/java/com/weatherfeed/app/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.weatherfeed.app.databinding.ActivityMainBinding
 
@@ -15,6 +16,8 @@ class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,20 +37,40 @@ class MainActivity : AppCompatActivity() {
         configureNavigation()
     }
 
+    private fun navigateTo(destinationId: Int) {
+        val options = androidx.navigation.navOptions {
+            popUpTo(navController.graph.startDestinationId) { saveState = true }
+            launchSingleTop = true
+            restoreState = true
+        }
+        navController.navigate(destinationId, null, options)
+    }
+
     private fun configureNavigation() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
 
-        val bottomNavigationView = binding.bottomNavigationView
+        navController = navHostFragment.navController
 
-        bottomNavigationView.setOnItemSelected { index ->
+        binding.bottomNavigationView.setOnItemSelected { index ->
             when (index) {
-                0 -> navController.navigate(R.id.homeFragment)
-                1 -> navController.navigate(R.id.forecastFragment)
-                2 -> navController.navigate(R.id.searchFragment)
-                3 -> navController.navigate(R.id.settingsFragment)
+                0 -> navigateTo(R.id.homeFragment)
+                1 -> navigateTo(R.id.forecastFragment)
+                2 -> navigateTo(R.id.searchFragment)
+                3 -> navigateTo(R.id.settingsFragment)
             }
+        }
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            val index = when (destination.id) {
+                R.id.homeFragment -> 0
+                R.id.forecastFragment -> 1
+                R.id.searchFragment -> 2
+                R.id.settingsFragment -> 3
+                else -> return@addOnDestinationChangedListener
+            }
+
+            binding.bottomNavigationView.setSelectedIndex(index)
         }
     }
 
